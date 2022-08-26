9b1cda5d01fed7240c6030e8cde372bf54192c41
thomas.bechet <thomas.bechet@univ-lyon1.fr>
2022-08-26 12:46:25 +0200

    fix debug/release config with mini3d

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <QMainWindow>
 
 #include "mainwindow.h"
+#include <mini3d.h>
 
 int main(int argc, char *argv[])
 {
+    auto *app = mini3d_app_new();
+    mini3d_app_delete(app);
+
     QApplication a(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
